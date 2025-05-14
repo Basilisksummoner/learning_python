f17a8fa8d4141163cdd5367dbbb8e33e0d6a4ac7
Nurt{AI} <pythonbasilisk@gmail.com>
2025-05-14 14:04:40 +0600

    Решил задачу с Code Wars
    Закрепляем problem solving

---
+++ mini-projects/Array+Array.py
@@ -0,0 +1,14 @@
+# Задача с CodeWars
+# Условие
+# I'm new to coding and now I want to get the sum of two arrays... 
+# Actually the sum of all their elements. I'll appreciate for your help.
+# P.S. Each array includes only integer numbers. Output is a number too.
+
+
+# Мое решение
+def array_plus_array(arr1, arr2):
+  return sum(arr1) + sum(arr2)
+
+a = [1,2,3]
+b = [1,2,3]
+print(array_plus_array(a,b))
